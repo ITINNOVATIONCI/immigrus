3a82e6d95d26b04365cf3ee37d3f1251566ad3c9
Florian Klinger <kflorian2157@hotmail.com>
2016-05-24 00:41:08 +0000

    2

---
+++ src/immigrus/Models/FlashInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace immigrus.Models
+{
+    public class FlashInfo
+    {
+        public string Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Ordre { get; set; }
+        public string Description { get; set; }
+        public string Etat { get; set; }
+    }
+}
